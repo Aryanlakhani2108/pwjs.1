9660b939c5b3a1a96e503bd060d69181538345c1
SHRODZ <137664693+SHRODZ@users.noreply.github.com>
2023-11-23 04:51:11 +0000

    discussion about variables

---
+++ basics1/variable.js
@@ -0,0 +1,11 @@
+//Variabls - let
+//Constants - const
+
+let userEmail = "Aryabdcnvdkfv@gmail.com";
+const password = "123"; 
+
+// console.log(password);
+
+password = "12234567";
+
+console.log(password);
