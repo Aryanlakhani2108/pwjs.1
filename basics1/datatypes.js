40cee6195f31d62d47b719accd19a70357af1f07
SHRODZ <137664693+SHRODZ@users.noreply.github.com>
2023-11-23 05:23:27 +0000

    we learnt about data types

---
+++ basics1/datatypes.js
@@ -0,0 +1,49 @@
+// Two categories of Data types in js
+
+// Primitive and Reference (Non - Primitive)
+
+// Primitive :
+/*
+Strings
+Numbers
+Boolean
+Null
+Undefined
+BigInt
+Symbol
+*/
+
+const fistname = "Lakhani";
+const age = 20;
+let isLoggedIn = true;
+const nodeVersion = null; 
+let bankBalance = undefined;
+const buttonId = Symbol("id");
+const aReallyLongNumber = 23874928372398n;
+
+/*
+Reference : 
+Arrays 
+Objects
+Function 
+*/
+
+const numbers = [1,2,3,4];
+const heroes = ["superman" , "batman" , "flash"];
+
+const userObject = { 
+    name : "Aryan",
+    roll : 21,
+    age : 19,
+    isLoggedIn = true
+}
+
+//function 
+const sayHello = function(){
+
+    console.log("Aryan");
+}
+
+const registered = function(){}
+const islive = function(){}
+
