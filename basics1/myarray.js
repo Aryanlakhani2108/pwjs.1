3d84f6b961f121b26267bac8b9cdfd300a75f5f1
SHRODZ <137664693+SHRODZ@users.noreply.github.com>
2023-11-24 05:52:14 +0000

    we learnt about array

---
+++ basics1/myarray.js
@@ -0,0 +1,16 @@
+const myArray = [1,2,3,4];
+const myNewArray = new Array(1,2,3,4,5,6,7);
+
+const dcHeroes = ["batman" , "superman" , "ironman"];
+dcHeroes.push("wonder woman");
+console.log(dcHeroes);
+dcHeroes.pop();
+console.log(dcHeroes);  
+
+// const lastHero = dcHeroes.pop();
+// console.log(lastHero);
+
+// the above method was used to contain the .pop() element in it
+
+
+
