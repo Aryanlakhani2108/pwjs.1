8acad9fb05364c2fe37d1e3fa06436bb1271c3a7
SHRODZ <137664693+SHRODZ@users.noreply.github.com>
2023-11-24 04:17:35 +0000

    we learnt about type conversion

---
+++ basics1/conversion.js
@@ -0,0 +1,17 @@
+let bankBalance = '123' ;
+
+let bankBalanceinInt = parseInt(bankBalance)  // Instead of parseInt() we can also use Number() 
+
+console.log( bankBalance)
+console.log( bankBalanceinInt) // output => 100
+
+// If string/number is empty(i.e does not mean it has 0 as a value it's value is undefined) then if we pass Boolean() if will give fasle output
+// If string/number is not empty then it will give output of true
+
+// let bankBalance = 'abc';
+
+// let bankBalanceinInt = parseInt(bankBalance)
+
+// console.log(bankBalanceinInt)
+
+// output => will be NaN
