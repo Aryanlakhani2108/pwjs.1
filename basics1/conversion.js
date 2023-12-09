6c9892231216f2007c7d46c5095d1a4401273d63
Aryan Lakhani <137664693+Aryanlakhani2108@users.noreply.github.com>
2023-12-09 04:22:15 +0000

    conversions in js

--- basics1/conversion.js
+++ basics1/conversion.js
@@ -8,10 +8,10 @@ console.log( bankBalanceinInt) // output => 100
 // If string/number is empty(i.e does not mean it has 0 as a value it's value is undefined) then if we pass Boolean() if will give fasle output
 // If string/number is not empty then it will give output of true
 
-// let bankBalance = 'abc';
+let bankBalance2 = 'abc';
 
-// let bankBalanceinInt = parseInt(bankBalance)
+let bankBalanceinInt2 = parseInt(bankBalance2)
 
-// console.log(bankBalanceinInt)
+console.log(bankBalanceinInt2)
 
 // output => will be NaN
